3a9e294855f0c89a76d2abcddfdad5e491d3cd21
Vasily-Kosikhin <dafmind@yandex.ru>
2022-06-08 17:52:30 +0300

    second

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
-
 import './index.scss';
+import Draggabilly from 'draggabilly';
 
-let Draggabilly = require('draggabilly');
+const draggie = new Draggabilly('.draggable', {});
 
-let draggie = new Draggabilly( '.draggable', {});
+// const movingTarget = document.querySelector(`.draggable`);
 
-const movingTarget = document.querySelector(`.draggable`);
+let startCoords = document.querySelector('.draggable')?.getBoundingClientRect();
 
-let startCoords;
-
-
-draggie.on( 'dragStart', getCoords );
-
-function getCoords() {
-    let target = event?.target as HTMLElement;
-    startCoords = target?.getBoundingClientRect();
+function getCoords(e: Event) {
+  const target = e?.target as HTMLElement;
+  startCoords = target?.getBoundingClientRect();
 }
 
-draggie.on( 'dragMove', showCoords );
+draggie.on('dragStart', getCoords);
 
-function showCoords() {
-    let target = event?.target as HTMLElement;
-    let shiftX = startCoords.left - target.getBoundingClientRect().left
-    let shiftY = startCoords.top - target.getBoundingClientRect().top
-    let range = Math.sqrt(Math.pow(shiftX, 2) + Math.pow(shiftY, 2)).toFixed(0)
-    target.innerHTML = `${range} px`
+function showCoords(e: Event) {
+  const target = e?.target as HTMLElement;
+  if (startCoords) {
+    const shiftX = startCoords.left - target.getBoundingClientRect().left;
+    const shiftY = startCoords.top - target.getBoundingClientRect().top;
+    const range = Math.sqrt(Math.pow(shiftX, 2) + Math.pow(shiftY, 2)).toFixed(
+      0
+    );
+    target.innerHTML = `${range} px`;
+  }
 }
+
+draggie.on('dragMove', showCoords);
